05be50118a8c080a4367b1607d2f6eeba5601fd8
Egor Lyubichev <EgorLyubichev@mail.ru>
2022-06-05 23:11:11 +0300

    Fixed bugs from exam

--- src/by/lev/controller/Registration.java
+++ src/by/lev/controller/Registration.java
@@ -28,7 +28,6 @@ public class Registration implements RegistrationInterface{
         writeRegistration(newUser);
         System.out.println("учетная запись успешно добавлена");
     }
-
     private void setCorrectLogin() {
         System.out.println("- - -| CACTUS CINEMA |- - -");
         System.out.println("- - -|  РЕГИСТРАЦИЯ  |- - -");
